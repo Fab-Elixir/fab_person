9d315ef364f9395e2bb2d51ee86b59a4c1753eae
Anthony Smith <420061+anthonator@users.noreply.github.com>
2025-05-07 18:10:49 -0400

    feat: implemented `Fab.Person`

---
+++ lib/fab/person.ex
@@ -0,0 +1,233 @@
+defmodule Fab.Person do
+  @moduledoc """
+  Functions for generating random information about a person.
+  """
+
+  import Fab.Locale
+  import Fab.Randomizer
+
+  @type sex_t ::
+          {:sex, :female | :male | :mixed}
+
+  @callback first_name([sex_t]) ::
+              [String.t()]
+
+  @callback full_name() ::
+              [String.t()]
+
+  @callback last_name([sex_t]) ::
+              [String.t()]
+
+  @callback middle_name([sex_t]) ::
+              [String.t()]
+
+  @callback prefix([sex_t]) ::
+              [String.t()]
+
+  @callback sex() ::
+              [String.t()]
+
+  @callback suffix() ::
+              [String.t()]
+
+  @optional_callbacks [
+    first_name: 1,
+    full_name: 0,
+    last_name: 1,
+    middle_name: 1,
+    prefix: 1,
+    sex: 0,
+    suffix: 0
+  ]
+
+  @doc """
+  Returns a random first name.
+
+  ## Options
+
+  - `:sex` – Can be `:female`, `:male`, or `:mixed`. Defaults to `:mixed`.
+
+  ## Examples
+
+      iex> Fab.Person.first_name()
+      "Ciara"
+
+      iex> Fab.Person.first_name(sex: :female)
+      "Cheryl"
+
+      iex> Fab.Person.first_name(sex: :male)
+      "Erik"
+  """
+  @spec first_name([sex_t]) :: String.t()
+  def first_name(opts \\ []) do
+    sex = Keyword.get(opts, :sex, :mixed)
+
+    __MODULE__
+    |> localize(:first_name, [[sex: sex]])
+    |> random()
+  end
+
+  @doc """
+  Generates a random full name.
+
+  ## Options
+
+  - `:first_name` - First name to use. Defaults to a randomly selected name
+  based on `:sex`.
+  - `:last_name` - Last name to use. Defaults to a randomly selected name based
+  on `:sex`.
+  - `:middle_name` - Middle name to use. Defaults to a randomly selected name
+  based on `:sex`.
+  - `:prefix` - Prefix to use. Defaults to a randomly selected prefix based on
+  `:sex`.
+  - `:sex` - Sex used to generate the first and last name. Can be `:female`,
+  `:male` or `:mixed`. Defaults to `:mixed`.
+  - `:suffix` - Suffix to use. Defaults to a randomly selected suffix based on
+  `:sex`.
+
+  ## Examples
+
+      iex> Fab.Person.full_name()
+      "Osbaldo Reilly"
+
+      iex> Fab.Person.full_name(first_name: "Anthony")
+      "Anthony Schuppe"
+
+      iex> Fab.Person.full_name(last_name: "Smith")
+      "Miss Orville Smith"
+
+      iex> Fab.Person.full_name(sex: :female)
+      "Dr. Amy Romaguera"
+
+      iex> Fab.Person.full_name(sex: :male)
+      "Mr. Bryan Reichert"
+  """
+  @spec full_name([sex_t]) :: String.t()
+  def full_name(opts \\ []) do
+    sex = Keyword.get(opts, :sex, :mixed)
+
+    bindings = [
+      prefix: Keyword.get(opts, :prefix, prefix(sex: sex)),
+      first_name: Keyword.get(opts, :first_name, first_name(sex: sex)),
+      middle_name: Keyword.get(opts, :middle_name, middle_name(sex: sex)),
+      last_name: Keyword.get(opts, :last_name, last_name(sex: sex)),
+      suffix: Keyword.get(opts, :suffix, suffix())
+    ]
+
+    __MODULE__
+    |> localize(:full_name, [])
+    |> random()
+    |> EEx.eval_string(bindings)
+  end
+
+  @doc """
+  Returns a random last name.
+
+  ## Options
+
+  - `:sex` – Can be `:female`, `:male`, or `:mixed`. Defaults to `:mixed`.
+
+  ## Examples
+
+      iex> Fab.Person.last_name()
+      "Reinger"
+
+      iex> Fab.Person.last_name(sex: :female)
+      "Lehner"
+
+      iex> Fab.Person.last_name(sex: :male)
+      "Glover"
+  """
+  @spec last_name([sex_t]) :: String.t()
+  def last_name(opts \\ []) do
+    sex = Keyword.get(opts, :sex, :mixed)
+
+    __MODULE__
+    |> localize(:last_name, [[sex: sex]])
+    |> random()
+  end
+
+  @doc """
+  Returns a random middle name.
+
+  ## Options
+
+  - `:sex` – Can be `:female`, `:male`, or `:mixed`. Defaults to `:mixed`.
+
+  ## Examples
+
+      iex> Fab.Person.middle_name()
+      "Jules"
+
+      iex> Fab.Person.middle_name(sex: :female)
+      "Ellen"
+
+      iex> Fab.Person.middle_name(sex: :male)
+      "William"
+  """
+  @spec middle_name([sex_t]) :: String.t()
+  def middle_name(opts \\ []) do
+    sex = Keyword.get(opts, :sex, :mixed)
+
+    __MODULE__
+    |> localize(:middle_name, [[sex: sex]])
+    |> random()
+  end
+
+  @doc """
+  Returns a random name prefix.
+
+  ## Options
+
+  - `:sex` – Can be `:female`, `:male`, or `:mixed`. Defaults to `:mixed`.
+
+  ## Examples
+
+      iex> Fab.Person.prefix()
+      "Mr."
+
+      iex> Fab.Person.prefix(sex: :female)
+      "Dr."
+
+      iex> Fab.Person.prefix(sex: :male)
+      "Mr."
+  """
+  @spec prefix([sex_t]) :: String.t()
+  def prefix(opts \\ []) do
+    sex = Keyword.get(opts, :sex, :mixed)
+
+    __MODULE__
+    |> localize(:prefix, [[sex: sex]])
+    |> random()
+  end
+
+  @doc """
+  Returns a random sex.
+
+  ## Examples
+
+      iex> Fab.Person.sex()
+      "male"
+  """
+  @spec sex :: String.t()
+  def sex do
+    __MODULE__
+    |> localize(:sex, [])
+    |> random()
+  end
+
+  @doc """
+  Returns a random name suffix.
+
+  ## Examples
+
+      iex> Fab.Person.suffix()
+      "DVM"
+  """
+  @spec suffix :: String.t()
+  def suffix do
+    __MODULE__
+    |> localize(:suffix, [])
+    |> random()
+  end
+end
---
+++ lib/fab/person/locales/az.ex
@@ -0,0 +1,316 @@
+defmodule Fab.Az.Person do
+  @moduledoc false
+
+  @behaviour Fab.Person
+
+  @impl Fab.Person
+  def first_name(opts) do
+    opts
+    |> Keyword.fetch!(:sex)
+    |> do_first_name()
+  end
+
+  @spec do_first_name(:female | :male | :mixed) :: [String.t()]
+  defp do_first_name(:female) do
+    [
+      "Adeliya",
+      "Afaq",
+      "Afərim",
+      "Afət",
+      "Aidə",
+      "Alsu",
+      "Anna",
+      "Ayan",
+      "Aydan",
+      "Aygül",
+      "Aygün",
+      "Aylin",
+      "Aynur",
+      "Aytən",
+      "Bahar",
+      "Banu",
+      "Billurə",
+      "Bəyaz",
+      "Cansu",
+      "Ceyla",
+      "Damla",
+      "Diana",
+      "Dilarə",
+      "Dəniz",
+      "Ella",
+      "Ellada",
+      "Elnarə",
+      "Elvira",
+      "Elyanora",
+      "Elza",
+      "Esmira",
+      "Estella",
+      "Fatimə",
+      "Fidan",
+      "Firuzə",
+      "Fərqanə",
+      "Fərəh",
+      "Fəxriyyə",
+      "Gövhər",
+      "Gülay",
+      "Gülsüm",
+      "Gülçin",
+      "Gülər",
+      "Günay",
+      "Humay",
+      "Hülya",
+      "Hüriyə",
+      "Jalə",
+      "Jasmin",
+      "Kübra",
+      "Lalə",
+      "Lamiyə",
+      "Laura",
+      "Leyla",
+      "Liliya",
+      "Ləman",
+      "Maya",
+      "Mehriban",
+      "Mələk",
+      "Nigar",
+      "Nuray",
+      "Nurgün",
+      "Nərgiz",
+      "Ofelya",
+      "Pəri",
+      "Röya",
+      "Selcan",
+      "Səbinə",
+      "Tansu",
+      "Tuba",
+      "Ulduz",
+      "Ülkər",
+      "Ülviyyə"
+    ]
+  end
+
+  defp do_first_name(:male) do
+    [
+      "Abbas",
+      "Abdulla",
+      "Adil",
+      "Afəl",
+      "Akif",
+      "Amid",
+      "Anar",
+      "Aqil",
+      "Bəhram",
+      "Bəhruz",
+      "Bəxtiyar",
+      "Elnur",
+      "Emil",
+      "Emin",
+      "Faiq",
+      "Kamran",
+      "Nadir",
+      "Nahid",
+      "Natiq",
+      "Nihad",
+      "Nurlan",
+      "Rafiq",
+      "Rəşad",
+      "Rəşid",
+      "Tahir",
+      "Tunar",
+      "Tərlan",
+      "Zahir",
+      "Zaur",
+      "İlham",
+      "İlqar",
+      "İxtiyar",
+      "Şaiq",
+      "Şəhriyar",
+      "Əhməd"
+    ]
+  end
+
+  defp do_first_name(:mixed) do
+    [
+      "Abbas",
+      "Abdulla",
+      "Adeliya",
+      "Adil",
+      "Afaq",
+      "Afəl",
+      "Afərim",
+      "Afət",
+      "Aidə",
+      "Akif",
+      "Alsu",
+      "Amid",
+      "Anar",
+      "Anna",
+      "Aqil",
+      "Ayan",
+      "Aydan",
+      "Aygül",
+      "Aygün",
+      "Aylin",
+      "Aynur",
+      "Aytən",
+      "Bahar",
+      "Banu",
+      "Billurə",
+      "Bəhram",
+      "Bəhruz",
+      "Bəxtiyar",
+      "Bəyaz",
+      "Cansu",
+      "Ceyla",
+      "Damla",
+      "Diana",
+      "Dilarə",
+      "Dəniz",
+      "Ella",
+      "Ellada",
+      "Elnarə",
+      "Elnur",
+      "Elvira",
+      "Elyanora",
+      "Elza",
+      "Emil",
+      "Emin",
+      "Esmira",
+      "Estella",
+      "Faiq",
+      "Fatimə",
+      "Fidan",
+      "Firuzə",
+      "Fərqanə",
+      "Fərəh",
+      "Fəxriyyə",
+      "Gövhər",
+      "Gülay",
+      "Gülsüm",
+      "Gülçin",
+      "Gülər",
+      "Günay",
+      "Humay",
+      "Hülya",
+      "Hüriyə",
+      "Jalə",
+      "Jasmin",
+      "Kamran",
+      "Kübra",
+      "Lalə",
+      "Lamiyə",
+      "Laura",
+      "Leyla",
+      "Liliya",
+      "Ləman",
+      "Maya",
+      "Mehriban",
+      "Mələk",
+      "Nadir",
+      "Nahid",
+      "Natiq",
+      "Nigar",
+      "Nihad",
+      "Nuray",
+      "Nurgün",
+      "Nurlan",
+      "Nərgiz",
+      "Ofelya",
+      "Pəri",
+      "Rafiq",
+      "Röya",
+      "Rəşad",
+      "Rəşid",
+      "Selcan",
+      "Səbinə",
+      "Tahir",
+      "Tansu",
+      "Tuba",
+      "Tunar",
+      "Tərlan",
+      "Ulduz",
+      "Zahir",
+      "Zaur",
+      "Ülkər",
+      "Ülviyyə",
+      "İlham",
+      "İlqar",
+      "İxtiyar",
+      "Şaiq",
+      "Şəhriyar",
+      "Əhməd"
+    ]
+  end
+
+  @impl Fab.Person
+  def full_name do
+    [
+      "<%= first_name %>",
+      "<%= first_name %> <%= last_name %>",
+      "<%= last_name %> <%= first_name %>"
+    ]
+  end
+
+  @impl Fab.Person
+  def last_name(opts) do
+    opts
+    |> Keyword.fetch!(:sex)
+    |> do_last_name()
+  end
+
+  @spec do_last_name(:female | :male | :mixed) :: [String.t()]
+  defp do_last_name(:female) do
+    [
+      "Abdullayeva",
+      "Qasımova",
+      "Rəşidova",
+      "Seyidova",
+      "Soltanova",
+      "Tahirova",
+      "Vəsiyeva",
+      "Əfəndiyeva",
+      "Əliyeva",
+      "Ələkbərova"
+    ]
+  end
+
+  defp do_last_name(:male) do
+    [
+      "Məmmədov",
+      "Nəzərov",
+      "Quliyev",
+      "Rəhimov",
+      "Soltanov",
+      "Vəliyev",
+      "Xəlilov",
+      "Əhmədov",
+      "Əliyev",
+      "Ələkbərov"
+    ]
+  end
+
+  defp do_last_name(:mixed) do
+    [
+      "Abdullayeva",
+      "Məmmədov",
+      "Nəzərov",
+      "Qasımova",
+      "Quliyev",
+      "Rəhimov",
+      "Rəşidova",
+      "Seyidova",
+      "Soltanov",
+      "Soltanova",
+      "Tahirova",
+      "Vəliyev",
+      "Vəsiyeva",
+      "Xəlilov",
+      "Əfəndiyeva",
+      "Əhmədov",
+      "Əliyev",
+      "Əliyeva",
+      "Ələkbərov",
+      "Ələkbərova"
+    ]
+  end
+end
